235b5864383e84ef6be1b506125300c8158fbf0f
Murtaza Akbari <murtazasakbari@gmail.com>
2024-02-11 21:32:00 +0100

    makes the headers reactive

--- src/events.rs
+++ src/events.rs
@@ -1,51 +1,59 @@
 use crate::FileState;
 use crate::GitIndex;
-use crossterm::{
-    event::{self, Event, KeyCode, KeyEventKind},
-};
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use ratatui::layout::Rect;
 use ratatui::text::{Line, Span};
 use ratatui::widgets::ListItem;
 use ratatui::{prelude::*, widgets::*, Frame};
 
-struct Staged<'a> {
-    title: &'a str,
+struct Header {
+    title: String,
     expanded: bool,
 }
 
-struct Unstaged<'a> {
-    title: &'a str,
-    expanded: bool,
+pub trait Toggle {
+    fn construct_title(&self) -> String;
+    fn toggle_section(&self) -> Header;
 }
 
-impl Default for Staged<'_> {
-    fn default() -> Self {
-        Staged {
-            title: "staged files",
-            expanded: false,
-        }
+impl Toggle for Header {
+    fn construct_title(&self) -> String {
+        let indicator = if self.expanded { "▼" } else { "▶" };
+        return format!("{} {}", indicator, self.title);
+    }
+
+    fn toggle_section(&self) -> Header {
+        return Header {
+            title: self.construct_title(),
+            expanded: !self.expanded,
+        };
     }
 }
 
-impl Default for Unstaged<'_> {
-    fn default() -> Self {
-        Unstaged {
-            title: "unstaged files",
-            expanded: false,
-        }
-    }
+pub struct AppState {
+    headers: Vec<Header>,
 }
 
-pub struct AppState<'a> {
-    staged: Staged<'a>,
-    unstaged: Unstaged<'a>,
-}
-
-impl AppState<'_> {
-    pub fn new() -> AppState<'static> {
+impl AppState {
+    pub fn new() -> AppState {
         AppState {
-            staged: Staged::default(),
-            unstaged: Unstaged::default(),
+            headers: vec![
+                Header {
+                    title: String::from("untracked files"),
+                    expanded: false,
+                }
+                .toggle_section(),
+                Header {
+                    title: String::from("unstaged files"),
+                    expanded: false,
+                }
+                .toggle_section(),
+                Header {
+                    title: String::from("staged files"),
+                    expanded: false,
+                }
+                .toggle_section(),
+            ],
         }
     }
 
@@ -67,11 +75,6 @@ impl AppState<'_> {
             }
         }
     }
-
-    fn toggle_section(&mut self) {
-        self.staged.expanded = !self.staged.expanded;
-        self.unstaged.expanded = !self.unstaged.expanded;
-    }
 }
 
 pub fn draw_ui<B: Backend>(
@@ -81,19 +84,7 @@ pub fn draw_ui<B: Backend>(
 ) -> Result<(), std::io::Error> {
     terminal.draw(|frame| {
         frame.set_cursor(0, 0);
-        let _header_state = draw_headers(
-            frame,
-            [
-                "▶  Untracked",
-                "▶  Unstaged",
-                "▶  Staged",
-                "▼  Untracked",
-                "▼  Unstaged",
-                "▼  Staged",
-            ],
-            frame.size(),
-            app_state,
-        );
+        let _header_state = draw_headers(frame, frame.size(), app_state);
     });
     Ok(())
 }
@@ -125,25 +116,22 @@ fn draw_files_section(frame: &mut Frame, area: &Rect, files: &[FileState]) {
     frame.render_widget(list, *area);
 }
 
-fn draw_headers(
-    frame: &mut Frame,
-    items: [&str; 6],
-    area: Rect,
-    _app_state: &AppState,
-) -> ListState {
+fn draw_headers(frame: &mut Frame, area: Rect, app_state: &AppState) -> ListState {
     let header_state: &mut ListState = &mut ListState::default();
-    let header_list = List::new(items)
+    let headers: Vec<ListItem> = app_state
+        .headers
+        .iter()
+        .map(|header| {
+            let text: Text = Text::from(header.title.clone());
+            ListItem::new(text)
+        })
+        .collect();
+
+    let header_list = List::new(headers)
         .block(Block::default().borders(Borders::ALL))
         .highlight_style(Style::new().add_modifier(Modifier::REVERSED))
         .repeat_highlight_symbol(true);
 
     frame.render_stateful_widget(header_list, area, header_state);
     return header_state.clone();
-}
-
-fn draw_header(frame: &mut Frame, area: &Rect, title: &str, expanded: bool) {
-    let symbol = if expanded { "▼" } else { "▶" };
-    let text = format!("{} {}", symbol, title);
-    let paragraph = Paragraph::new(text).style(Style::default().fg(Color::Yellow));
-    frame.render_widget(paragraph, *area);
 }
